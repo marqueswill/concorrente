ad67aefb18393f2a2fef51317baecf3ab80984c4
willyan <willyanmarquesmelo@gmail.com>
2025-09-11 11:01:55 -0300

    Changes to be committed: 	deleted:    macacos 	new file:   macacos_cond.c 	renamed:    macacos] -> macacos_mutex 	modified:   macacos_mutex.c

--- macacos_mutex.c
+++ macacos_mutex.c
@@ -3,43 +3,48 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-#define MA 10  // macacos que andam de A para B
-#define MB 10  // macacos que andam de B para A
+#define MA 10 // macacos que andam de A para B
+#define MB 10 // macacos que andam de B para A
 
 pthread_mutex_t lock_corda = PTHREAD_MUTEX_INITIALIZER;
-pthread_cond_t cond_ab = PTHREAD_COND_INITIALIZER;
-pthread_cond_t cond_ba = PTHREAD_COND_INITIALIZER;
 
 int num_macacosAB = 0;
 int num_macacosBA = 0;
 int golira_quer_passar = 0;
+int ultimo_lado_aberto = 'B'; // Variável para controlar o lado que passou por último
 
-char lado_golira = 'A';  // lado em que o golira está
+// Desnecessário já que já estamos contando os macacos passando de um lado pra outro
+// int BA_esperando = 1;
+// int AB_esperando = 0;
+
+char lado_golira = 'A'; // lado em que o golira está
 
 void *macacoAB(void *a) {
     int i = *((int *)a);
-    while (1) {
-        // Se existem macacos passando de B para A, bloqueia a passagem de macacos de A para B
-        // Se o golira está passando, bloqueia a passagem de todos os macacos
-        pthread_mutex_lock(&lock_corda);
-            while (num_macacosBA || golira_quer_passar) {  // While para garantir que ao acordar da condição, a situação ainda é válida
-                pthread_cond_wait(&cond_ab, &lock_corda);
-            }
-            num_macacosAB++;
-        pthread_mutex_unlock(&lock_corda);
+    while (1) { // Loop principal do macaco
+       
+        while (1) {                                                             
+            pthread_mutex_lock(&lock_corda);                                                      // Loop de espera para entrar na corda, as threads ficam bloqueando e desbloqueando em loop                         
+                if (num_macacosBA == 0 && golira_quer_passar == 0 && ultimo_lado_aberto == 'B') { // Se não há macacos indo de B para A e o gorila não quer passar, eu deixo as threads passarem
+                    num_macacosAB++;                                                              // O controle do último lado aberto faz com que as threads AB e BA se alternem, evitando que um lado fique preso esperando o outro acabar. Funciona por conta da contagem de macacos
+                    pthread_mutex_unlock(&lock_corda);                                            // Libero a corda para os macacos passarem
+                    break;                                                                        // Sai do loop, agora as trheads AB podem continuar
+                }
+            pthread_mutex_unlock(&lock_corda);
+            sleep(1);
+        }
 
         printf("Macaco %02d passado de A para B. Macacos AB: %02d. Macacos BA: %02d\n", i, num_macacosAB, num_macacosBA);
         sleep(1);
 
-        // Procedimentos para quando sair da corda
-        pthread_mutex_lock(&lock_corda);  // protege a variável num_macacos_corda
+        // Sai da corda
+        pthread_mutex_lock(&lock_corda);
             num_macacosAB--;
+            ultimo_lado_aberto = 'A';
             printf("Macaco %02d saiu da corda. Macacos AB: %02d. Macacos BA: %02d\n", i, num_macacosAB, num_macacosBA);
-            if (num_macacosAB == 0) {
-                pthread_cond_broadcast(&cond_ba);  // Libera todos os macacos que estão esperando para passar de B para A
-            }
         pthread_mutex_unlock(&lock_corda);
-        sleep(3); // esperar um pouco do outro lado
+
+        sleep(1); // Esperar um pouco do outro lado pra não dar ruim
     }
     pthread_exit(0);
 }
@@ -47,63 +52,59 @@ void *macacoAB(void *a) {
 void *macacoBA(void *a) {
     int i = *((int *)a);
     while (1) {
-        // Se existem macacos passando de B para A, bloqueia a passagem de macacos de A para B
-        // Se o golira está passando, bloqueia a passagem de todos os macacos
-        pthread_mutex_lock(&lock_corda);
-            while (num_macacosAB || golira_quer_passar) {  // While para garantir que ao acordar da condição, a situação ainda é válida
-                pthread_cond_wait(&cond_ba, &lock_corda);
-            }
-            num_macacosBA++;
-        pthread_mutex_unlock(&lock_corda);
 
-        printf("Macaco %02d passado de A para B. Macacos AB: %02d. Macacos BA: %02d\n", i, num_macacosAB, num_macacosBA);
+        while (1) { // Loop de espera para entrar na corda
+            pthread_mutex_lock(&lock_corda);
+                if (num_macacosAB == 0 && golira_quer_passar == 0 && ultimo_lado_aberto == 'A') {
+                    num_macacosBA++;
+                    pthread_mutex_unlock(&lock_corda);
+                    break; // Sai do loop
+                }
+            pthread_mutex_unlock(&lock_corda);
+            sleep(1);
+        }
+
+        printf("Macaco %02d passado de B para A. Macacos AB: %02d. Macacos BA: %02d\n", i, num_macacosAB, num_macacosBA);
         sleep(1);
 
-        // Procedimentos para quando sair da corda
-        pthread_mutex_lock(&lock_corda);  // protege a variável num_macacos_corda
+        // Sai da corda
+        pthread_mutex_lock(&lock_corda);
             num_macacosBA--;
+             ultimo_lado_aberto = 'B';
             printf("Macaco %02d saiu da corda. Macacos AB: %02d. Macacos BA: %02d\n", i, num_macacosAB, num_macacosBA);
-
-            if (num_macacosBA == 0) {
-                pthread_cond_broadcast(&cond_ab);  // Libera todos os macacos que estão esperando para passar de B para A
-            }
         pthread_mutex_unlock(&lock_corda);
-        sleep(3); // esperar um pouco do outro lado
+
+        sleep(1); // Esperar um pouco do outro lado
     }
     pthread_exit(0);
 }
 
 void *golira(void *a) {
     while (1) {
-        // Procedimentos para acessar a corda
         
-        pthread_mutex_lock(&lock_corda);
-            while (num_macacosAB || num_macacosBA) {        // While para garantir que ao acordar da condição, a situação ainda é válida
-                golira_quer_passar = 1;      
-                printf("GOLIRA QUER PASSAR DE %c \n", lado_golira);
-
-                if (lado_golira == 'A') { // Se o golira está em A, espera os macacos de A para B sairem
-                    pthread_cond_wait(&cond_ba, &lock_corda);
-                } else {                  // Senao, espera os macacos de B para A sairem
-                    pthread_cond_wait(&cond_ab, &lock_corda);   
+        while (1) { // Loop de espera para o gorila atravessar
+            golira_quer_passar = 1;
+            pthread_mutex_lock(&lock_corda);
+                if (num_macacosAB == 0 && num_macacosBA == 0) {
+                    pthread_mutex_unlock(&lock_corda);
+                    break; // Sai do loop
                 }
-            }
+            pthread_mutex_unlock(&lock_corda);
+            sleep(1);
+        }
 
-
+        pthread_mutex_lock(&lock_corda);
             if (lado_golira == 'A') {
-                printf("GOLIRA PASSANO de A para B \n");
+                printf("GOLIRA PASSANO de A para B\n");
                 lado_golira = 'B';
             } else {
-                printf("GOLIRA PASSANO de B para A \n");
+                printf("GOLIRA PASSANO de B para A\n");
                 lado_golira = 'A';
             }
-
             golira_quer_passar = 0;
-
         pthread_mutex_unlock(&lock_corda);
 
-        sleep(5);
-
+        sleep(5); // Tempo que o gorila leva para atravessar e descansar
     }
     pthread_exit(0);
 }
