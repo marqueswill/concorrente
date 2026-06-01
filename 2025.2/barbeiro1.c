a00bdebb88ef8a2f6da650d72ff64927fe73c37d
Willyan Marques <willyanmarquesmelo@gmail.com>
2026-06-01 13:03:43 -0300

    Changes to be committed: 	modified:   .vscode/tasks.json 	modified:   2025.2/barbeiro1.c 	modified:   2026.1/resolucoes/res_barbeiro_sem.c

--- 2025.2/barbeiro1.c
+++ 2025.2/barbeiro1.c
@@ -1,11 +1,11 @@
 /*
  * Problema do barbeiro dorminhoco.
  */
-#include <pthread.h>
-#include <semaphore.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
+#include "pthread.h"
+#include "semaphore.h"
+#include "stdio.h"
+#include "stdlib.h"
+#include "unistd.h"
 
 #define N_CLIENTES 15
 #define N_CADEIRAS 5
